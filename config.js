d86780d27c83693b9663335a69c47e7c305fc55b
beltah <isaac0maina@gmail.com>
2025-07-27 17:55:27 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+
+global.owner = ['254741819582'] // your WhatsApp number
+global.botName = 'LostBoy MD'
+global.prefix = '.'
+global.sessionName = 'session'
+global.autoRead = true
+global.autoTyping = true
+global.autoRecord = false
+global.available = true
