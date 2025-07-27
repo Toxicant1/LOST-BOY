d46e8ef18c268d74bb846a25b2c9bfb07d078f42
beltah <isaac0maina@gmail.com>
2025-07-27 08:53:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ async function authentication() {
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 if(!session) return console.log('Please add your session to SESSION env !!')
 const sessdata = session.replace("BLACK MD;;;", '');
-const filer = await File.fromURL(https://mega.nz/file/${sessdata})
+const filer = await File.fromURL(`https://mega.nz/file/${sessdata}`);
+
 filer.download((err, data) => {
 if(err) throw err
 fs.writeFile(__dirname + '/sessions/creds.json', data, () => {
